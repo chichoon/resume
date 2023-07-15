89380c6096d62f123073551e0d11817b484d56d0
chichoon <chichoon.choi@gmail.com>
2023-07-15 20:19:17 +0900

    fix: :art: metadata 수정, reset.css 불러오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,6 @@ import { getCertificates, getFootprints, getProfile, getProjects } from '@/servi
 
 import styles from './resumePage.module.scss';
 
-export const metadata: Metadata = {
-  title: '최지윤의 이력서',
-  description: '지금까지의 이력을 나타낸 이력서입니다.',
-  keywords: 'Resume Page',
-  publisher: '최지윤',
-  creator: '최지윤',
-};
-
 const Page = async () => {
   const profileData = await getProfile();
   const projectData = await getProjects();
