c6f50e1244aefaeda4fc4dc9e3949313b106a708
chichoon <chichoon.choi@gmail.com>
2023-07-16 22:34:34 +0900

    style: :lipstick: 헤더 스타일 조정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { ProfileType } from '@/types/profileData';
 import { HeaderElement } from './HeaderElement';
 
 import styles from './header.module.scss';
+import Image from 'next/image';
 
 interface Props {
   profileData: ProfileType;
@@ -10,15 +11,23 @@ interface Props {
 export const Header = ({ profileData }: Props) => {
   return (
     <header className={styles.headerWrapper}>
-      <h1>{profileData.name}</h1>
-      <HeaderElement title='이메일' value={profileData.email} link={`mailto:${profileData.email}`} />
-      <HeaderElement title='전화번호' value={process.env.NEXT_PUBLIC_PROFILE_PHONE_NBR} />
-      <HeaderElement
-        title='GitHub'
-        value={`https://github.com/${profileData.githubUsername}`}
-        link={`https://github.com/${profileData.githubUsername}`}
-      />
-      <HeaderElement title='블로그' value={profileData.blogLink} link={profileData.blogLink} />
+      <div className={styles.headerDividerLeft}>
+        <Image src={profileData.profileSrc} alt='profile photo' width={200} height={200} />
+        <div className={styles.headerDivider}>
+          <h1>{profileData.name}</h1>
+          <span>프론트엔드 개발자</span>
+        </div>
+      </div>
+      <div className={styles.headerDividerRight}>
+        <HeaderElement title='이메일' value={profileData.email} link={`mailto:${profileData.email}`} />
+        <HeaderElement title='전화번호' value={process.env.NEXT_PUBLIC_PROFILE_PHONE_NBR} />
+        <HeaderElement
+          title='GitHub'
+          value={`https://github.com/${profileData.githubUsername}`}
+          link={`https://github.com/${profileData.githubUsername}`}
+        />
+        <HeaderElement title='블로그' value={profileData.blogLink} link={profileData.blogLink} />
+      </div>
     </header>
   );
 };
